a5706fdf8d3f434220928d3fff6af14571426c9a
Luis F. Mieses Gómez <42974051+luismieses@users.noreply.github.com>
2020-05-10 17:36:35 -0400

    Update syntax.ml

--- final-proj/syntax.ml
+++ final-proj/syntax.ml
@@ -1,1 +1,21 @@
 (* Escribir AST aqui  *)
+type binop = Plus | Minus | Mult | And | Or | Less
+            | LessEq | Greater | GreaterEq | Eq | Ne
+
+type expr =
+  | Num of int64
+  | Add1 of expr
+  | Sub1 of expr
+  | BinOp of expr * binop * expr
+  | Id of string
+  | Bool of bool
+  | Let of string * expr * expr
+  | If of expr * expr * expr
+  | Not of expr
+
+type decl =
+  | Func of string * string list * expr
+
+type program =
+  | Decls of decl list * expr
+  | Expr of expr
